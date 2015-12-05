b92b733c4e8a002672b2dd203748cc3b57980c47
Zach Riggle <riggle@google.com>
2015-12-05 17:16:54 -0500

    Clean up a bit

--- main.c
+++ main.c
@@ -11,14 +11,6 @@ int main(int argc, char** argv) {
 
     chroot_add_uid_mapping(getuid(), getuid());
     chroot_add_gid_mapping(getgid(), getgid());
-
-    // // Map all supplementary groups
-    // gid_t groups[128];
-    // size_t n_gids = getgroups(128, groups);
-    // for(size_t i = 0; i < n_gids; i++) {
-    //     chroot_add_gid_mapping(groups[i], groups[i]);
-    // }
-
     chroot_add_bind_defaults();
     chroot_add_bind("home", "/home", 0);
     chroot_invoke(argv[1]);
